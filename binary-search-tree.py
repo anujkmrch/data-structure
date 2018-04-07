b858c1cf768463c3d702653e1cbc565c409325e2
Anuj Kumar Chaudhary <kumar_anuj87@yahoo.co.in>
2018-04-07 06:05:09 +0100

    First binary tree and binary search tree

---
+++ binary-search-tree.py
@@ -0,0 +1,58 @@
+'''
+Auto adjust binary tree
+'''
+#Start Node class
+class Node(object):
+    """docstring for Node."""
+
+    def __init__(self, arg,name):
+        super(Node, self).__init__()
+        self.left = None
+        self.right = None
+        self.root = arg
+        self.name = name
+
+    # add value to the binary search tree
+    # at correct position according to the weight
+    def addvalue(self,arg,name):
+        if arg < self.root:
+            if self.left == None:
+                self.left = Node(arg,name)
+                return
+            self.left.addvalue(arg,name)
+
+        if arg >self.root:
+            if self.right == None:
+                self.right = Node(arg,name)
+                return
+            self.right.addvalue(arg,name)
+
+    ## Method to return the tree value
+    ## according to the weight
+    def getValue(self,arg):
+        if arg < self.root:
+            return self.left.getValue(arg)
+
+        if arg > self.root:
+            return self.right.getValue(arg)
+
+        return self
+#End Node class
+
+root = Node(6,"A")
+root.addvalue(5,"B")
+root.addvalue(4,"F")
+root.addvalue(3,"D")
+root.addvalue(2,"J")
+root.addvalue(1,"I")
+root.addvalue(9,"C")
+root.addvalue(7,"G")
+root.addvalue(8,"K")
+root.addvalue(10,"H")
+
+##demo loop to search the weight value
+for i in range(0,13):
+    try:
+        print("Element with weight {0} has value : {1}".format(i,root.getValue(i).name))
+    except Exception as e:
+        print("Weight \"{}\" has no value".format(i))
---
+++ binary-tree.py
@@ -0,0 +1,115 @@
+'''
+You can check your results from here,
+in my case k and i are missing
+you may add it to your result
+Anuj Kumar <me@anujkch.com>
+http://btechsmartclass.com/DS/U3_T4.html
+'''
+#Node class Start
+class Node(object):
+    """docstring for Node."""
+    def __init__(self, weight,name):
+        super(Node, self).__init__()
+        self.left = None
+        self.right = None
+        self.weight = weight
+        self.name = name
+    '''
+      method to add left element to the current node
+     '''
+    def addLeft(self, node):
+            self.left = node
+
+#    method to add right element to the current node
+    def addRight(self, node):
+        self.right = node
+
+#Node class end
+
+#Print in order
+def printInOrder(arg):
+    if(arg != None):
+        printInOrder(arg.left)
+        print arg.weight,arg.name
+        printInOrder(arg.right)
+
+#Print Pre order tree
+def printPreOrder(arg):
+    if(arg != None):
+        print arg.weight,arg.name
+        printPreOrder(arg.left)
+        printPreOrder(arg.right)
+
+#Print Post Order binary tree
+def printPostOrder(arg):
+    if(arg != None):
+        printPostOrder(arg.left)
+        printPostOrder(arg.right)
+        print arg.weight,arg.name
+
+
+'''
+                                  (5,A) #Root Node
+                                /      \
+                               /        \
+                              /          \
+                       #left(4,B)        (6,C)#right
+                            /   \          /    \
+                           /     \        /      \
+              #left_left(2,D)  (3,F)     (8,G)    (9,H)#right_right
+                        /  \  #left_right   \#right_left
+                       /    \                \
+      #left_left_left(0,I)  (1,J)             (10,K)#right_left_right
+                            #left_left_right
+
+                                        '''
+
+
+#Root node
+root = Node(5,"A")
+#Left node of root node
+left = Node(4,"B")
+
+#Right node of root's left node
+left_right = Node(3,"F")
+
+left_left = Node(2,"D")
+
+left_left_right = Node(1,"J")
+
+left_left_left = Node(0,"I")
+
+right = Node(6,"C")
+
+right_left = Node(8,"G")
+
+right_left_right = Node(10,"K")
+
+right_right = Node(9,"H")
+
+left_left.addLeft(left_left_left)
+
+left_left.addRight(left_left_right)
+
+left.addLeft(left_left)
+
+left.addRight(left_right)
+
+right_left.addRight(right_left_right)
+
+right.addLeft(right_left)
+
+right.addRight(right_right)
+
+root.addLeft(left)
+
+root.addRight(right)
+
+print "--- IN ---"
+printInOrder(root)
+
+print "--- PRE ---"
+printPreOrder(root)
+print "---POST---"
+printPostOrder(root)
+print "------"
